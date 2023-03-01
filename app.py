b7662ca19b40e8f1739de7f27da24971ab2d170a
zyzyzyclaire <kunji117@naver.com>
2023-03-01 20:08:20 +0900

    0301

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask,render_template
+from models import session_scope, User
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    with session_scope() as session:
+        test = session.query(User).filter(User.id == 1).first()
+    return render_template('/index.html', test=test)
 
 if __name__ == '__main__':
     app.run()
--- models.py
+++ models.py
@@ -1,12 +1,20 @@
-from sqlalchemy import BIGINT, Column, Integer, BigInteger, String, Text, Date,Boolean, DateTime, ForeignKey, Enum, Index, UniqueConstraint, Float, Numeric, ForeignKeyConstraint
+from sqlalchemy import Column, Integer, BigInteger, String, Text, Date, Time, DateTime, ForeignKey
 from werkzeug.security import generate_password_hash
 
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from contextlib import contextmanager
+from sqlalchemy.sql import func
+  
+from sqlalchemy.dialects.mysql import BINARY
+from sqlalchemy.types import TypeDecorator
 
-from config import DATABASE_USER, DATABASE_PASSWORD, DATABASE_HOST, DATABASE_PORT, DATABASE_DB
+from config import DATABASE_HOST, DATABASE_DB, DATABASE_PORT, DATABASE_PASSWORD, DATABASE_USER
+
+import json
+import enum
+import datetime
 
 url = 'mysql://{}:{}@{}:{}/{}'.format(
     DATABASE_USER,
@@ -34,21 +42,175 @@ def session_scope():
         session.close()
 
 
-# increase 넘버 조정
+
+class Template(Base):
+    __tablename__ = 'template'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(50), nullable=False)
+    description = Column(String(256), nullable=True)
+
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+
+
 class User(Base):
     __tablename__ = 'user'
-    __table_args__ = (UniqueConstraint('hotel_id', 'ota_type', 'name'),)
     id = Column(BigInteger, primary_key=True, autoincrement=True)
-    hotel_id = Column(Integer, ForeignKey('hotel.id'), nullable=False)
-    ota_type = Column(Enum(OTAType), nullable=False)
     name = Column(String(128), nullable=False)
-    created_at = Column(DateTime, nullable=False, default=datetime.datetime.utcnow)
-    updated_at = Column(DateTime, nullable=False, default=datetime.datetime.utcnow)
+    user_id = Column(String(512), nullable=False, unique=True)
+    user_pw = Column(String(512), nullable=False)
+    email = Column(String(512), nullable=False, unique=True)
+    template_id = Column(Integer, ForeignKey('template.id'), default=None)
 
-    def __init__(self, hotel_id, ota_type, name):
-        self.hotel_id = hotel_id
-        self.ota_type = ota_type
+    def __init__(self, name, user_id, user_pw, email, template_id):
         self.name = name
+        self.user_id = user_id
+        self.user_pw = generate_password_hash(user_pw)
+        self.email = email
+        self.template_id = template_id
+
+
+# class Account(Base):
+#     __tablename__ = 'account'
+#     id = Column(Integer, primary_key=True, autoincrement=True)
+#     acc_bank = Column(String(50), nullable=False)
+#     acc_number = Column(String(50), nullable=False)
+#     acc_name = Column(String(50), nullable=False)
+#     group_name = Column(String(50), nullable=False)
+#     user_id = Column(BigInteger, ForeignKey('user.id'), nullable=False)
+
+#     def __init__(self, acc_bank, acc_number, acc_name, group_name, user_id):
+#         self.acc_bank = acc_bank
+#         self.acc_number = acc_number
+#         self.acc_name = acc_name
+#         self.group_name = group_name
+#         self.user_id = user_id
+
+
+# class Relation(Base):
+#     __tablename__ = 'relation'
+#     id = Column(Integer, primary_key=True, autoincrement=True)
+#     relation = Column(String(50), nullable=False)
+
+#     def __init__(self, relation):
+#         self.relation = relation
+
+
+# class Guestbook(Base):
+#     __tablename__ = 'guest_book'
+#     id = Column(Integer, primary_key=True, autoincrement=True)
+#     writer = Column(String(50), nullable=False)
+#     writer_pw = Column(String(50), nullable=False)
+#     created_at = Column(DateTime, nullable=False, default=datetime.datetime.utcnow)
+#     contents = Column(Text, nullable=False)
+#     user_id = Column(BigInteger, ForeignKey('user.id'), nullable=False)
+
+#     def __init__(self, writer, writer_pw, contents, user_id):
+#         self.writer = writer
+#         self.writer_pw = writer_pw
+#         self.contents = contents
+#         self.user_id = user_id
+
+
+# class Information(Base):
+#     __tablename__ = 'information'
+#     id = Column(Integer, primary_key=True, autoincrement=True)
+#     first_name = Column(String(50), nullable=False)
+#     last_name = Column(String(50), nullable=False)
+#     tel = Column(String(50), nullable=True)
+#     relation_id = Column(BigInteger, ForeignKey('user.id'), nullable=False)
+
+#     def __init__(self, first_name, last_name, tel, relation_id):
+#         self.first_name = first_name
+#         self.last_name = last_name
+#         self.tel = tel
+#         self.relation_id = relation_id
+
+
+# class Picturetype(Base):
+#     __tablename__ = 'picture_type'
+#     id = Column(Integer, primary_key=True, autoincrement=True)
+#     name = Column(String(50), nullable=False)
+
+#     def __init__(self, name):
+#         self.name = name
+
+
+# class Picture(Base):
+#     __tablename__ = 'picture'
+#     id = Column(Integer, primary_key=True, autoincrement=True)
+#     url = Column(String(50), nullable=False)
+#     user_id = Column(BigInteger, ForeignKey('user.id'), nullable=False)
+#     picture_type = Column(Integer, ForeignKey('picture.id'), nullable=False)
+
+#     def __init__(self, first_name, last_name, picture_type):
+#         self.first_name = first_name
+#         self.last_name = last_name
+#         self.picture_type = picture_type
+
+
+# class Texttype(Base):
+#     __tablename__ = 'text_type'
+#     id = Column(Integer, primary_key=True, autoincrement=True)
+#     name = Column(String(50), nullable=False)
+
+#     def __init__(self, name):
+#         self.name = name
+
+
+# class Text(Base):
+#     __tablename__ = 'text'
+#     id = Column(Integer, primary_key=True, autoincrement=True)
+#     contents = Column(Text, nullable=False)
+#     user_id = Column(BigInteger, ForeignKey('user.id'), nullable=False)
+#     text_type = Column(Integer, ForeignKey('text_type.id'), nullable=False)
+
+#     def __init__(self, contents, user_id, text_type):
+#         self.contents = contents
+#         self.user_id = user_id
+#         self.text_type = text_type
+
+
+# class Transportationtype(Base):
+#     __tablename__ = 'transportation_type'
+#     id = Column(Integer, primary_key=True, autoincrement=True)
+#     name = Column(String(50), nullable=False)
+
+#     def __init__(self, name):
+#         self.name = name
+
+
+# class Transportation(Base):
+#     __tablename__ = 'transportation'
+#     id = Column(Integer, primary_key=True, autoincrement=True)
+#     contents = Column(Text, nullable=False)
+#     user_id = Column(BigInteger, ForeignKey('user.id'), nullable=False)
+#     transportation_type = Column(Integer, ForeignKey('transportation_type.id'), nullable=False)
+
+#     def __init__(self, contents, user_id, transportation_type):
+#         self.contents = contents
+#         self.user_id = user_id
+#         self.transportation_type = transportation_type
+
+
+# class Weddinghall(Base):
+#     __tablename__ = 'wedding_hall'
+#     id = Column(Integer, primary_key=True, autoincrement=True)
+#     name = Column(String(128), nullable=False)
+#     address = Column(String(256), nullable=False, unique=True)
+#     address_detail = Column(String(256), nullable=False, unique=True)
+#     date = Column(Date, nullable=False)
+#     time = Column(Time, nullable=False)
+#     user_id = Column(Integer, ForeignKey('user.id'), default=None)
+
+#     def __init__(self, name, address, address_detail, date, time, user_id):
+#         self.name = name
+#         self.address = address
+#         self.address_detail = address_detail
+#         self.date = date
+#         self.time = time
+#         self.user_id = user_id
 
 
 Base.metadata.create_all(engine)
